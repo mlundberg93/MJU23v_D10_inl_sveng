a1ff748bde30e86789cfbaa7580cd87cacbc9a42
mlundberg93 <Marcusluundbergh@gmail.com>
2023-11-03 10:20:59 +0100

    Kollat igenom koden, testkört programmet för att hitta eventuella fel eller exceptions. Lagt till flera kommentarer.

--- Program.cs
+++ Program.cs
@@ -25,13 +25,13 @@
                 Console.Write("> ");
                 string[] argument = Console.ReadLine().Split();
                 string command = argument[0];
-                if (command == "quit")
+                if (command == "quit") //NYI: Programmet avslutas inte när man kör kommando Quit.
                 {
                     Console.WriteLine("Goodbye!");
                 }
                 else if (command == "load")
                 {
-                    if(argument.Length == 2)
+                    if(argument.Length == 2)    //Unhandled exception. System.IO.FileNotFoundException: Could not find file, felhantera fel filläsning.
                     {
                         using (StreamReader sr = new StreamReader(argument[1]))
                         {
@@ -62,7 +62,7 @@
                 }
                 else if (command == "list")
                 {
-                    foreach(SweEngGloss gloss in dictionary)
+                    foreach(SweEngGloss gloss in dictionary)  //FIXME: Unhandled exception. System.NullReferenceException, när jag kör list före load i console.
                     {
                         Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
                     }
@@ -76,10 +76,10 @@
                     else if(argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string s = Console.ReadLine();  //FIXME: ändra till ett mer bekripligt variabel namn
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
-                        dictionary.Add(new SweEngGloss(s, e));
+                        string e = Console.ReadLine();  //FIXME: ändra till ett mer bekripligt variabel namn
+                        dictionary.Add(new SweEngGloss(s, e)); //FIXME: Unhandled exception. System.NullReferenceException: Object reference not set to an instance of an object.
                     }
                 }
                 else if (command == "delete")
@@ -92,16 +92,18 @@
                             if (gloss.word_swe == argument[1] && gloss.word_eng == argument[2])
                                 index = i;
                         }
-                        dictionary.RemoveAt(index);
+                        dictionary.RemoveAt(index); //FIXME: Unhandled exception. System.ArgumentOutOfRangeException: Index was out of range.
+                                                    //Ifall man inte stavar ordet rätt vid delete.
                     }
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string s = Console.ReadLine();  //FIXME: ändra till ett mer bekripligt variabel namn
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
+                        string e = Console.ReadLine();  //FIXME: ändra till ett mer bekripligt variabel namn
                         int index = -1;
-                        for (int i = 0; i < dictionary.Count; i++)
+                        for (int i = 0; i < dictionary.Count; i++) //FIXME: Unhandled exception. System.NullReferenceException: Object reference not set to an instance of an object.
+                                                                   //Om man kör Delete utan ladda listan.
                         {
                             SweEngGloss gloss = dictionary[i];
                             if (gloss.word_swe == s && gloss.word_eng == e)
@@ -125,7 +127,7 @@
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word to be translated: ");
-                        string s = Console.ReadLine();
+                        string s = Console.ReadLine();  //FIXME: ändra till ett mer bekripligt variabel namn
                         foreach (SweEngGloss gloss in dictionary)
                         {
                             if (gloss.word_swe == s)
@@ -134,6 +136,7 @@
                                 Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
                         }
                     }
+                    //NYI: Skapa en hjälp funktion med utskrift över olika kommandon
                 }
                 else
                 {
